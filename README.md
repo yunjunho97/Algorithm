136d90d9ef4ea65bdf95f7a1659578808f5c880e
yunjunho97 <yunjunho97.gm@gmail.com>
2024-05-11 17:19:17 +0900

    대소문자 변환

---
+++ README.md
@@ -0,0 +1,16 @@
+### 1. 문자열 관련  
+toUpperCase: 대문자로 변환  
+toLowerCase: 소문자로 변환  
+```
+str = str.toUpperCase();
+c = Character.toUpperCase();
+```
+<br/>
+
+isUpperCase: 대문자면 true, 소문자면 false  
+isLowerCase: 대문자면 false, 소문자면 true
+```
+Character.isUpperCase('A'); //true
+Character.isUpperCase('a'); //false
+```
+<br/>
--- src/inflearn/Test2_CaseConversion.java
+++ src/inflearn/Test2_CaseConversion.java
@@ -8,7 +8,7 @@ public class Test2_CaseConversion {
         Test2_CaseConversion caseConversion = new Test2_CaseConversion();
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        System.out.println(caseConversion.solution(input));
+        System.out.println(caseConversion.solution2(input));
 
     }
 
@@ -17,8 +17,28 @@ public class Test2_CaseConversion {
         for(char c : str.toCharArray()){
             if(Character.isUpperCase(c)){
                 answer += Character.toLowerCase(c);
+                //대문자라면 소문자로 변환
             } else {
                 answer += Character.toUpperCase(c);
+                //소문자라면 대문자로 변환
+            }
+        }
+        return answer;
+    }
+
+    //아스키코드를 활용한 방법
+    //A~Z: 65~90
+    //a~z: 97~122
+    //대문자와 소문자의 차이는 32
+    public static String solution2(String str){
+        String answer = "";
+        for(char c : str.toCharArray()){
+            if(c >= 65 && c <= 97){
+                answer += (char)(c+32);
+                //대문자라면 소문자로 변환
+            } else {
+                answer += (char)(c-32);
+                //소문자라면 대문자로 변환
             }
         }
         return answer;
