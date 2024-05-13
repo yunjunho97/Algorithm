b9b85cf8bcd261b24eeeca994d8b94086d2c2964
yunjunho97 <yunjunho97.gm@gmail.com>
2024-05-13 17:47:19 +0900

    문자열 뒤집기

--- src/inflearn/Test4_ReverseString.java
+++ src/inflearn/Test4_ReverseString.java
@@ -1,5 +1,6 @@
 package inflearn;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test4_ReverseString {
@@ -20,6 +21,18 @@ public class Test4_ReverseString {
         }
 
         System.out.println(solution(count, str));
+
+        /////////////////////////
+        //solution2
+        for(String x : solution2(count, str)){
+            System.out.println(x);
+        }
+
+        /////////////////////////
+        //solution3
+        for(String x : solution3(count, str)){
+            System.out.println(x);
+        }
     }
 
     public static String solution(int count, String str[]) {
@@ -29,7 +42,7 @@ public class Test4_ReverseString {
         for (int i = 0; i < count; i++) {
             String empty = "";
 
-            //char값들을 stack형식으로 새 문자열에 하나씩 추가
+            //char값들을 제일 앞부터 stack처럼 새 문자열에 하나씩 추가
             for (char c : str[i].toCharArray()) {
                 empty = c + empty;
             }
@@ -37,10 +50,56 @@ public class Test4_ReverseString {
             answer[i] = empty;
         }
 
+        //출력을 위해 하나의 문자열로 합침
         for (int i = 0; i < count; i++) {
             answer2 += answer[i] + '\n';
         }
 
         return answer2;
+    }
+
+    //StringBuilder 사용
+    public static ArrayList<String> solution2(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(String x: str){
+            //String은 String끼리 더하거나 replace같은걸 하면 새로운 객체가 생성된다
+            //StringBuilder은 객체를 여러개 만들지 않고 그대로 사용
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    //제일 앞과 끝에 포인터를 각각 두고 포인터를 증감시키며 교체하는 방식
+    public static ArrayList<String> solution3(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for(String x : str){
+            char[] tmp = x.toCharArray();
+
+            //pointer 초기화
+            int leftPointer = 0;
+            int rightPointer = tmp.length - 1;
+
+            while(leftPointer < rightPointer) {
+                //두 pointer가 가리키고 있는 값을 서로 교환
+                char temp = tmp[leftPointer];
+                tmp[leftPointer] = tmp[rightPointer];
+                tmp[rightPointer] = temp;
+
+                //pointer 증가, 감소
+                leftPointer++;
+                rightPointer--;
+            }
+
+            //char배열을 String값으로 변환
+            String temp2 = String.valueOf(tmp);
+
+            answer.add(temp2);
+        }
+
+        return answer;
     }
 }
