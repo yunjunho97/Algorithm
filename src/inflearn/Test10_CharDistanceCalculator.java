995355e7b45c61dddd1f9b4e0d30d4e545ff9a6c
yunjunho97 <yunjunho97.gm@gmail.com>
2024-05-15 14:10:07 +0900

    문자와 문자간의 최소 거리

--- src/inflearn/Test10_CharDistanceCalculator.java
+++ src/inflearn/Test10_CharDistanceCalculator.java
@@ -7,8 +7,11 @@ public class Test10_CharDistanceCalculator {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         System.out.println(solution(input));
+        System.out.println(solution2(input));
     }
 
+    //문자열에서 특정 문자와 각 문자들 간의 최소 거리
+    //입력예시: teachermode e
     public static String solution(String inputString){
         String answer = "";
         String strA = inputString.split(" ")[0]; // 대상 문자열
@@ -42,6 +45,49 @@ public class Test10_CharDistanceCalculator {
         }
 
         //출력 형식을 위한 전처리
+        for(int x : list){
+            answer += x + " ";
+        }
+
+        return answer;
+    }
+
+    //
+    public static String solution2(String inputString){
+        String answer = "";
+
+        String str = inputString.split(" ")[0];
+        char target = inputString.split(" ")[1].charAt(0);
+        int[] list = new int[str.length()];
+
+        //왼쪽에서 오른쪽으로 출발
+        //거리의 초기값을 1000으로 설정
+        int count = 1000;
+        for(int i = 0; i < str.length(); i++){
+            //target을 만나면 count를 0으로 하고 list에 저장
+            if(str.charAt(i) == target){
+                count = 0;
+            //다른 문자를 만나면 count값을 증가시키고 list에 저장
+            } else {
+                count++;
+            }
+            list[i] = count;
+        }
+
+        //오른쪽에서 왼쪽으로 다시한번 출발
+        count = 1000;
+        for(int i = str.length() - 1; i >= 0; i--){
+            if(str.charAt(i) == target){
+                count = 0;
+            } else {
+                count++;
+            }
+            //이미 list에 있는 거리값과 비교해서 더 작은것을 저장
+            if(list[i] > count) {
+                list[i] = count;
+            }
+        }
+
         for(int x : list){
             answer += x + " ";
         }
