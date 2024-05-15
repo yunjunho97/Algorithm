091ae53be4dbbb27c6bf9e3ae27d36f61f175732
yunjunho97 <yunjunho97.gm@gmail.com>
2024-05-15 14:09:42 +0900

    문자열에서 숫자만 추출, isDigit() 사용

--- src/inflearn/Test9_NumberExtraction.java
+++ src/inflearn/Test9_NumberExtraction.java
@@ -9,9 +9,22 @@ public class Test9_NumberExtraction {
         System.out.println(solution(input));
     }
 
+    //문자열에서 숫자만 추출
     public static int solution(String input){
         input = input.replaceAll("[^0-9]", "");
         int answer = Integer.parseInt(input);
         return answer;
     }
+
+    //isDigit() 사용
+    public static int solution2(String input){
+        String answer = "";
+        for(char x : input.toCharArray()){
+            if(Character.isDigit(x)){
+                answer += x;
+            }
+        }
+        return Integer.parseInt(answer);
+    }
+
 }
