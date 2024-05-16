9150e42cc1fed84faea1ecfc1cd2f212006c5576
yunjunho97 <yunjunho97.gm@gmail.com>
2024-05-16 19:29:33 +0900

    10, 2, 8, 16진수 변환

---
+++ src/library_memo/NumberBase_memo.java
@@ -0,0 +1,34 @@
+package library_memo;
+
+public class NumberBase_memo {
+    public static void main(String[] args) {
+
+        int decimal = 10;
+        //10진수 -> 2진수
+        String binary = Integer.toBinaryString(decimal);
+        System.out.println("binary = " + binary);
+
+        //10진수 -> 8진수
+        String octal = Integer.toOctalString(decimal);
+        System.out.println("octal = " + octal);
+
+        //10진수 -> 16진수
+        String hex = Integer.toHexString(decimal);
+        System.out.println("hex = " + hex);
+
+        //2진수 -> 10진수
+        String binary_ = "11";
+        int decimalByBinary = Integer.parseInt(binary_, 2);
+        System.out.println("decimalByBinary = " + decimalByBinary);
+
+        //8진수 -> 10진수
+        String octal_ = "11";
+        int decimalByOctal = Integer.parseInt(octal_, 8);
+        System.out.println("decimalByOctal = " + decimalByOctal);
+
+        //16진수 -> 10진수
+        String hex_ = "11";
+        int decimalByHex = Integer.parseInt(hex_, 16);
+        System.out.println("decimalByHex = " + decimalByHex);
+    }
+}
