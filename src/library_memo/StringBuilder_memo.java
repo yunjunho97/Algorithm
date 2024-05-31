bcefbfbb16a924dd8ee2b6f898ca32c1af4706e5
yunjunho97 <yunjunho97.gm@gmail.com>
2024-05-31 17:40:42 +0900

    StringBuilder 내용 추가

--- src/library_memo/StringBuilder_memo.java
+++ src/library_memo/StringBuilder_memo.java
@@ -7,6 +7,14 @@ public class StringBuilder_memo {
         StringBuilder stringBuilder = new StringBuilder("0123456");
 
         //0. toString() StringBuilder의 내용을 String으로 변환하여 반환
+        //StringBuilder 는 내부적으로 char[] 배열을 통해 문자열을 저장하고 관리한다
+        //StringBuilder 객체가 생성될 때 배열이 초기 크기로 할당되고
+        //기존 크기보다 더 많은 문자열이 추가되면 새로운 더 큰 배열이 할당되고 기존 배열의 내용은 새로운 배열로 복사된다
+        //기존 크기보다 작아지는 경우에는 배열을 축소하는 작업을 하지는 않는다, 크기는 변경되지 않고 그대로 유지된다
+        //내부 배열의 사용된 부분만 변경이 된다
+
+        //내부 배열의 크기를 명시적으로 줄이려면 trimToSize()를 사용
+        //현재 문자열의 길이에 맞게 내부 배열의 크기를 조정
 
         //1. 제일 끝에 추가
         String temp1 = stringBuilder.append("append").toString();
