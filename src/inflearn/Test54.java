4f89b01af2ef99024d738d825ec7ea444609ff0a
yunjunho97 <yunjunho97.gm@gmail.com>
2024-06-12 20:41:28 +0900

    합이 같은 부분집합

--- src/inflearn/Test54.java
+++ src/inflearn/Test54.java
@@ -18,19 +18,19 @@ public class Test54 {
     }
 
     public static void solution(int[] arr){
-        total = 0;
+        total = 0; //배열 전체의 합
         for(int x : arr){
             total += x;
         }
         solution2(0, 0, arr);
     }
     public static void solution2(int l, int sum, int[] arr){
-        if(total-sum == sum) flag = true;
+        if(total-sum == sum) flag = true; //전체합의 절반이면 성공
 
-        if(flag) return;
-        if(l == arr.length) return;
+        if(flag) return; //성공한 뒤 스택에 남아있는 함수들 다 리턴
+        if(l == arr.length) return; //배열의 끝까지 가면 리턴
 
-        solution2(l+1, sum + arr[l], arr);
-        solution2(l+1, sum, arr);
+        solution2(l+1, sum + arr[l], arr); //인덱스 l의 수를 더하는 경우
+        solution2(l+1, sum, arr); //인덱스 l의 수를 더하지 않는 경우
     }
 }
