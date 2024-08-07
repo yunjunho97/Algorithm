347e23868c2a796f3ed8b25cf4bef21ec57e123d
yunjunho97 <yunjunho97.gm@gmail.com>
2024-08-07 17:43:24 +0900

    프로그래머스 - 행렬의 곱셈

---
+++ src/programmers/Test80.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Test80 {
+    public static void main(String[] args) {
+        int[][] arr1 = {{2,3,2},{4,2,4},{3,1,4}};
+        int[][] arr2 = {{5,4,3},{2,4,1},{3,1,1}};
+        int[][] answer = solution(arr1, arr2);
+        for(int[] a : answer){
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
+    public static int[][] solution(int[][] arr1, int[][] arr2){
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0; i < arr1.length; i++){
+            for(int j = 0; j < arr2[0].length; j++){
+                int sum = 0;
+                for(int k = 0; k < arr1[0].length; k++){
+                    sum += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = sum;
+            }
+        }
+        return answer;
+    }
+}
